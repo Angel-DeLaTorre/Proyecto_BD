ae8cab02f1e1afce4591bfdbdb588b2bbc61a28c
Brallan Leonardo Sanchez Villalobos <villalobosleonardo41@gmail.com>
2020-04-11 19:13:54 -0500

    cree varios archivos para el login aun falta manejar tipos de usuarios, creo que sera bueno crear un home controler para cada usuario

--- Proyecto_BD/Datos/Conexion.cs
+++ Proyecto_BD/Datos/Conexion.cs
@@ -20,9 +20,9 @@ namespace Proyecto_BD.Datos
         {
 
             this.Base = "DB_Materialab"; //Nombre de la base de datos
-            this.Servidor = "ANTONIO-TORRES\\SQLEXPRESS"; //Servidor
-            this.Usuario = "SystemCSharp"; //Usuario
-            this.Clave = "?system2019?"; //Contraseña
+            this.Servidor = "DESKTOP-TIV58TO\\SQLEXPRESS"; //Servidor
+            this.Usuario = "sa"; //Usuario
+            this.Clave = "?root?"; //Contraseña
             this.Seguridad = true; // Con esta variable se decide el tipo de seguridad de la bd
         }
         public SqlConnection CrearConexion()
