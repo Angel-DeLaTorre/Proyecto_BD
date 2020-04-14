ebf975afefcb8aa87f6a20d45d6c52b05182383c
Angel de la Torre <angel.delatorre.glez@gmail.com>
2020-04-14 14:34:53 -0500

    Cambios

--- Proyecto_BD/Datos/Conexion.cs
+++ Proyecto_BD/Datos/Conexion.cs
@@ -19,9 +19,9 @@ namespace Proyecto_BD.Datos
         private Conexion()
         {
             this.Base = "DB_Materialab"; //Nombre de la base de datos
-            this.Servidor = "LAPTOP-RU2M13JD\\MARIO"; //Servidor
-            this.Usuario = "sa"; //Usuario
-            this.Clave = "1234"; //Contraseña
+            this.Servidor = "LAPTOP_ANGEL"; //Servidor
+            this.Usuario = "angel"; //Usuario
+            this.Clave = "admin"; //Contraseña
             this.Seguridad = true; // Con esta variable se decide el tipo de seguridad de la bd
         }
         public SqlConnection CrearConexion()
