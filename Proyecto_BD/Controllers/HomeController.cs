741ab2954c2819f33884273f83b6ad9153ebac70
Angel de la Torre <angel.delatorre.glez@gmail.com>
2020-04-14 14:08:40 -0500

    Cambios login, y dashboard

--- Proyecto_BD/Controllers/HomeController.cs
+++ Proyecto_BD/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Proyecto_BD.Datos;
+using Proyecto_BD.Models;
+using System.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace Proyecto_BD.Controllers
 {
@@ -10,6 +14,7 @@ namespace Proyecto_BD.Controllers
     {
         public ActionResult Index()
         {
+            cargarDatos();
             return View();
         }
 
@@ -26,5 +31,31 @@ namespace Proyecto_BD.Controllers
 
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Index(string date, string labor)
+        {
+            // Aquí cualquier uso de las variables 'usr', 'pwd' y 'rme'
+            ViewBag.dateF = date;
+            ViewBag.laborF = labor;
+            List<string[]> listaReporte = DHome.ListarReporte(date, labor);
+            ViewBag.listaReporte = listaReporte;
+            cargarDatos();
+            return View("Index");
+        }
+
+        private void cargarDatos() {
+            List<Prestamo> listPrestamos = DHome.ListarAlumnosPendientes3();
+            ViewBag.ListaPres3 = listPrestamos;
+            int countMaterial = DHome.countMateriales();
+            ViewBag.countMaterial = countMaterial;
+            int countPrestamo = DHome.countPrestamosRetrasos();
+            ViewBag.countPrestamo = countPrestamo;
+            List<string[]> listExistencias = DHome.ListarExistenciaLab();
+            ViewBag.listExistencias = listExistencias;
+            List<Laboratorio> listLab = DHome.listLaboratorios();
+            ViewBag.listLab = listLab;
+        }
+
     }
 }
