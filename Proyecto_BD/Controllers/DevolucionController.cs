a1b024736cfca3db11b9c533778a8adaabfd817d
Angel de la Torre <angel.delatorre.glez@gmail.com>
2020-04-15 20:13:08 -0500

    q

--- Proyecto_BD/Controllers/DevolucionController.cs
+++ Proyecto_BD/Controllers/DevolucionController.cs
@@ -11,8 +11,8 @@ namespace Proyecto_BD.Controllers
         // GET: Devolucion
         public ActionResult Index()
         {
+            ViewBag.listPrestamo = DDevolucion.getEjemplaresPrestados("20000003");
             return View();
-
         }
     }
 }
